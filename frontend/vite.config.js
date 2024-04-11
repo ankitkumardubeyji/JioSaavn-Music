90277cc3c4df722901feceaf317aa0a70ecab899
Ankit kumar Dubey <122801994+ankitkumardubeyji@users.noreply.github.com>
2024-04-11 07:23:58 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
       proxy:{
-        '/api':'jio-saavn-music1.vercel.app'
+        '/api':'https://jio-saavn-music1.vercel.app'
       }
   },
   plugins: [react()],
